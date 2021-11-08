ed8434fd840fa6315b6177065c3487efc62db218
Espana01 <alex.espana.mt@correo.usa.edu.co>
2021-11-08 16:24:15 -0500

    Retos final

---
+++ src/main/java/tutoria/Repositorio/AudienceRepositorio.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tutoria.Repositorio;
+
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import tutoria.Modelo.Audience;
+import tutoria.Interface.InterfaceAudience;
+
+/**
+ *
+ * @author USUARIO
+ */
+@Repository
+public class AudienceRepositorio {
+      @Autowired
+    private InterfaceAudience crud;
+    
+
+    public List<Audience> getAll(){
+        return (List<Audience>) crud.findAll();       
+    }
+    
+    public Optional <Audience> getAudience(int id){
+        return crud.findById(id);
+    }
+    
+    public Audience save(Audience audience){
+        return crud.save(audience);
+    }
+     public void delete(Audience audience){
+        crud.delete(audience);
+    }
+    
+}
